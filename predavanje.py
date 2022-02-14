b668d6293735b70d1ee25c7b248bf657bb9aa23e
klemenramsak <klemza1@gmail.com>
2022-02-14 13:23:21 +0100

    Initial commit

---
+++ predavanje.py
@@ -0,0 +1,35 @@
+# x = 5
+# y = 2
+# print(x + y)
+
+# some_string = "Hey there!"
+# print(some_string)
+
+# string_a = "Hello"
+# string_b = "World!"
+# print(string_a + string_b)
+# print(string_a + " " + string_b)
+
+# bool_one = True
+# bool_two = False
+# some_var = None
+
+# user_name = input("Please enter your name: ")
+# print("Hello " + user_name + "!")
+# first_num = int(input("Enter the first number: "))
+# second_num = int(input("Enter the second number: "))
+# print(first_num + second_num)
+
+# mood = "happy"
+# if mood == "happy":
+#     print("It is great to see you happy!")
+# else:
+#     print("Cheer up, mate!")
+
+mood = "nervous"
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+else:
+    print("Cheer up, mate!")
